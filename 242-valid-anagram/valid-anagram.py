f3b198eedb14a65e5e88be839e413387dfd0e362
Pakhi Srivastava <pakhisrivastava2122@gmail.com>
2026-04-18 21:46:28 +0530

    Time: 15 ms (43.24%) | Memory: 20.2 MB (17.27%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+
+        # freq = {}
+        # for i in s:
+        #     if i not in freq:
+        #         freq[i] = 1
+        #     else:
+        #         freq[i] += 1
+
+        # for i in t:
+        #     if i not in freq:
+        #         return False
+        #     else:
+        #         freq[i] -= 1
+
+        # for i in freq.values():
+        #     if i != 0:
+        #         return False
+
+        # return True
+        # BRUTE FORCE APPROACH 
+
+        if len(s)!=len(t):
+            return False
+        
+        sort_s=sorted(s)
+        sort_t=sorted(t)
+        if sort_s==sort_t:
+            return True 
+        return False
