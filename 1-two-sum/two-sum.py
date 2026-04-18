2aac33a17051105c7abd73253f32361b0be9e752
Pakhi Srivastava <pakhisrivastava2122@gmail.com>
2026-04-18 18:06:03 +0530

    Time: 4 ms (39.43%) | Memory: 20.5 MB (46.51%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -7,15 +7,24 @@ class Solution:
         #         return [s[complement], index]
         #     s[num] = index
         # return []
-        n = len(nums)
+        
+        # BRUTE FORCE APPROACH  
+        # n = len(nums)
        
-        for i in range(n-1):
-            for j in range(i+1,n):
-                if nums[i]+nums[j]==target:
-                    return([i,j])
+        # for i in range(n-1):
+        #     for j in range(i+1,n):
+        #         if nums[i]+nums[j]==target:
+        #             return([i,j])
             
 
-
+# OPTIMAL APPROACHHH 
+        n=len(nums)
+        hash_set={}
+        for i in range(0,n):
+            remaining = target-nums[i]
+            if remaining in hash_set:
+                return [hash_set[remaining],i]
+            hash_set[nums[i]]=i
 
 
                 
